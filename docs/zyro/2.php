dc17bdac528b702ba522ddf5260bda7032dccd34
Anil Jami <anil.jami@oportun.com>
2021-05-03 17:45:03 -0700

    init

---
+++ docs/zyro/2.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Anil weds Ramya</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.39" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1485259412" rel="stylesheet" type="text/css" />
+	<link href="css/2.css?ts=1485259412" rel="stylesheet" type="text/css" />
+	<script src="js/jquery.browser.min.js" type="text/javascript"></script>
+	<link href="js/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" type="text/css" />
+	<script src="js/fancybox/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
+	
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance0" class="wb_element"><ul class="hmenu"><li class="active"><a href="Home/" target="_self" title="Home">Home</a></li><li class="active"><a href="About/" target="_self" title="About">About</a></li><li class="active"><a href="Gallery/" target="_self" title="Gallery">Gallery</a></li><li class="active"><a href="Contacts/" target="_self" title="Contacts">Contacts</a></li></ul></div><div id="wb_element_instance1" class="wb_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle">Dream wedding</h4>
+</div><div id="wb_element_instance2" class="wb_element"><img alt="logo" src="gallery_gen/8e8711946ef6f69ce6fda848413af357_60x60.png"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance5" class="wb_element"><img alt="we" src="gallery_gen/b94e51af54856fe94204a9c1a0e9711a_230x130.jpg"><script type="text/javascript">
+				$("#wb_element_instance5").fancybox({
+					href: "gallery_gen/b94e51af54856fe94204a9c1a0e9711a_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance6" class="wb_element"><img alt="we" src="gallery_gen/b94e51af54856fe94204a9c1a0e9711a_228x130.jpg"><script type="text/javascript">
+				$("#wb_element_instance6").fancybox({
+					href: "gallery_gen/b94e51af54856fe94204a9c1a0e9711a_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance7" class="wb_element"><img alt="we" src="gallery_gen/b94e51af54856fe94204a9c1a0e9711a_220x125.jpg"><script type="text/javascript">
+				$("#wb_element_instance7").fancybox({
+					href: "gallery_gen/b94e51af54856fe94204a9c1a0e9711a_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance8" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><a href="Home/">Engagement pic.....</a><br>
+ </p>
+
+<p class="wb-stl-normal">This is our engagement pic</p>
+</div><div id="wb_element_instance9" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><a href="Home/" style="background-color: rgb(255, 250, 235);">Engagement pic.....</a><br>
+ </p>
+
+<p class="wb-stl-normal">This is our engagement pic</p>
+</div><div id="wb_element_instance10" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><a href="Home/" style="background-color: rgb(255, 250, 235);">Engagement pic.....</a></p>
+
+<p class="wb-stl-normal">This is our engagement pic</p>
+</div><div id="wb_element_instance11" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><a href="Home/" style="background-color: rgb(255, 250, 235);">Engagement pic.....</a></p>
+
+<p class="wb-stl-normal">This is our engagement pic</p>
+</div><div id="wb_element_instance12" class="wb_element"><img alt="Anil" src="gallery_gen/e865b53fd726ef3b809091c623cff65e_340x340.jpg"><script type="text/javascript">
+				$("#wb_element_instance12").fancybox({
+					href: "gallery_gen/e865b53fd726ef3b809091c623cff65e_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance13" class="wb_element"><img alt="We" src="gallery_gen/73c4512aa48493c458c83c961ada0fbe_340x340.jpg"><script type="text/javascript">
+				$("#wb_element_instance13").fancybox({
+					href: "gallery_gen/73c4512aa48493c458c83c961ada0fbe_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance14" class="wb_element"><img alt="Ramya" src="gallery_gen/3b0a488d163d0de92d3113d90c4e468a_340x340.jpg"><script type="text/javascript">
+				$("#wb_element_instance14").fancybox({
+					href: "gallery_gen/3b0a488d163d0de92d3113d90c4e468a_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance15" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;">Ramya</h1>
+</div><div id="wb_element_instance16" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;">We</h1>
+</div><div id="wb_element_instance17" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;">Anil</h1>
+</div><div id="wb_element_instance18" class="wb_element" style=" line-height: normal;"><h4 class="wb-stl-pagetitle">Gallery</h4>
+</div><div id="wb_element_instance19" class="wb_element"><div style="font-size: 1px; overflow: hidden; line-height: 1px; padding: 0; background: transparent; float: none; position: relative; margin: 1px 0 0 0; width: 100%; height: 1px; left: 0; top: 50%; border-top: 1px solid #e0d8c0;"></div></div><div id="wb_element_instance20" class="wb_element"><img alt="we" src="gallery_gen/b94e51af54856fe94204a9c1a0e9711a_228x130.jpg"><script type="text/javascript">
+				$("#wb_element_instance20").fancybox({
+					href: "gallery_gen/b94e51af54856fe94204a9c1a0e9711a_fancybox.jpg",
+					"hideOnContentClick": true
+				});
+			</script></div><div id="wb_element_instance21" class="wb_element" style=" line-height: normal;"><p>.</p>
+</div><div id="wb_element_instance22" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(2);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance22");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance22").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 157px;">
+	
+<div id="wb_element_instance3" class="wb_element"><a href="fb.com/anil.jami2"><img alt="fb" src="gallery_gen/e732cda2fadad544bf4af3cba88f7248_48x48.png"></a></div><div id="wb_element_instance4" class="wb_element"><a href="http://fb.com/anil.jami2"><img alt="tw" src="gallery_gen/8bdce7be406b2c472c0e1ec542a143de_48x48.png"></a></div><div id="wb_element_instance23" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.height(106);
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div></body>
+</html>
